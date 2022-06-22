dd6e50b93c528eef45127406f90d25cac2f9f844
Kittiphop Pitiwittayakul <kittiphop293@gmail.com>
2022-06-22 09:19:34 +0700

    first commit

---
+++ components/logo.js
@@ -0,0 +1,20 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const logo = () => {
+  return (
+    <View>
+      <Text style={styles.testLogo} >TNI</Text>
+    </View>
+  )
+}
+
+export default logo
+
+const styles = StyleSheet.create({
+    testLogo:{
+        color:"red",
+        fontSize:40
+        
+    }
+})
---
+++ components/styles.js
@@ -0,0 +1,20 @@
+import { StyleSheet } from "react-native-web";
+const styles = StyleSheet.create({
+    container:{
+      flex:1,
+      backgroundColor:"#33ccff",
+      alignItems:'center',
+      justifyContent:'center',
+    },
+  
+  title:{
+  fontSize: 20,
+    },
+  warning:{
+    color: 'green'
+  }
+  
+  });
+
+  export default styles
+  
