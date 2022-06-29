48a3d8d2f69edd63b6a8cc06f12c0b74f2ef61b0
Kittiphop Pitiwittayakul <kittiphop293@gmail.com>
2022-06-29 11:06:25 +0700

    JSX

--- components/User.js
+++ components/User.js
@@ -1,23 +1,36 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text } from "react-native";
+import React from "react";
 
 const User = () => {
-    const users = [
-        {id:1,name:"John"},
-        {id:2,name:"Jeb"}
-    ];
+  const users = [
+    { id: 1, name: "John" },
+    { id: 2, name: "Jeb" },
+  ];
   return (
-    <View >
+    <View>
       <Text></Text>
-      {
-        users.map((user,index)=>{
-            return <Text key={user.id}> {index+1}. {user.name} </Text>
-
-
-        })
-      }
+      {users.map((user, index) => {
+        return (
+          <Text
+          style={{color:"red",
+          fontSize:40,
+          backgroundColor: "#61dafb",
+          padding: 10,
+          width:700,
+          textAlign: "center",
+          marginBottom: 50,
+          paddingVertical: 8,
+          borderWidth: 4,
+          borderRadius: 6,
+  }}
+          key={user.id}>
+            {" "}
+            {index + 1}. {user.name}{" "}
+          </Text>
+        );
+      })}
     </View>
-  )
-}
+  );
+};
 
-export default User
+export default User;
